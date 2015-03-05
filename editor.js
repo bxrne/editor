d79f82b743382c8dd73871a5b9cbfdcd112f573b
Adam Byrne <adam.r.byrne@gmail.com>
2015-03-05 21:12:58 +0000

    added autocomplete

--- editor.js
+++ editor.js
@@ -1,6 +1,17 @@
 var editor = ace.edit("editor");
 var preview =document.getElementById('preview');
+document.getElementById('editor').style.fontSize='16px';
 
+ace.require("ace/ext/language_tools");
+    var editor = ace.edit("editor");
+    editor.session.setMode("ace/mode/html");
+    editor.setTheme("ace/theme/tomorrow");
+    // enable autocompletion and snippets
+    editor.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true
+    });
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/html");
 editor.setHighlightActiveLine(true);
