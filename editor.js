cf29216e295b28c4ff8bc8124a3811163eda7202
Adam Byrne <adam.r.byrne@gmail.com>
2015-03-06 08:10:54 +0000

    added Options(emmet and language tools) with better styling(margin/padding)

--- editor.js
+++ editor.js
@@ -3,15 +3,15 @@ var preview =document.getElementById('preview');
 document.getElementById('editor').style.fontSize='16px';
 
 ace.require("ace/ext/language_tools");
-    var editor = ace.edit("editor");
-    editor.session.setMode("ace/mode/html");
-    editor.setTheme("ace/theme/tomorrow");
-    // enable autocompletion and snippets
-    editor.setOptions({
-        enableBasicAutocompletion: true,
-        enableSnippets: true,
-        enableLiveAutocompletion: true
-    });
+ace.require("ace/ext/emmet");
+
+editor.setOptions({
+  enableBasicAutocompletion: true,
+  enableSnippets: true,
+  enableLiveAutocompletion: false,
+  enableEmmet: true
+});
+
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/html");
 editor.setHighlightActiveLine(true);
